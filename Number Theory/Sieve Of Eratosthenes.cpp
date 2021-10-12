fed8118d6ab9a64ed32be2ed393f7fd46b3e0f52
Sabrina Mostafa <78508655+sabrina-mostafa@users.noreply.github.com>
2021-10-12 23:00:48 +0600

    Update Sieve Of Eratosthenes.cpp

--- Number Theory/Sieve Of Eratosthenes.cpp
+++ Number Theory/Sieve Of Eratosthenes.cpp
@@ -3,36 +3,86 @@ using namespace std ;
 #define mx 40000
 int mark[40000] ;
 
-int main()
+void sieve()
 {
-    int n, i, j ;
-    int primes[40000], x=1 ;
-    cin>>n ;
-    mark[0] = 1 ;
+    int primes[40000] ;
+    int i, j, x=1 ;
+    mark[0] =1 ;
     mark[1] = 1 ;
-    for(i=4; i*i<=mx; i+=2)
-        mark[i] = 1 ;
 
-    for(i=3; i*i<=mx; i+=2)
+    for(i=2; i*i<=mx; i++)
     {
         if(mark[i]==0)
         {
-            for(j=i*i; j<=mx; j+= 2*i )
+            for(j=i*i; j<=mx; j+=i )
+            {
+                mark[j] = 1 ;
+            }
+        }
+    }
+#include<bits/stdc++.h>
+using namespace std ;
+#define mx 40000
+int mark[40000] ;
+
+void sieve()
+{
+    int primes[40000] ;
+    int i, j, x=1 ;
+    mark[0] =1 ;
+    mark[1] = 1 ;
+
+    for(i=2; i*i<=mx; i++)
+    {
+        if(mark[i]==0)
+        {
+            for(j=i*i; j<=mx; j+=i )
             {
                 mark[j] = 1 ;
             }
         }
     }
 
-    /*
+/*
     for(i=0; i<=mx; i++)
      {
              if(mark[i]==0) primes[x++]=i ;
      }
      for(i=1; i<x; i++)
              cout<<primes[i]<<" " ;
-             */
+             cout<<endl ;
+*/
+}
 
-             if(mark[n]==0) cout<<"Prime number " ;
-             else cout<<"Not a prime number " ;
+int main()
+{
+    int n, i, j ;
+    cin>>n ;
+    sieve() ;
+
+
+    if(mark[n]==0) cout<<"Prime number " ;
+    else cout<<"Not a prime number " ;
+}
+
+/*
+    for(i=0; i<=mx; i++)
+     {
+             if(mark[i]==0) primes[x++]=i ;
+     }
+     for(i=1; i<x; i++)
+             cout<<primes[i]<<" " ;
+             cout<<endl ;
+*/
+}
+
+int main()
+{
+    int n, i, j ;
+    cin>>n ;
+    sieve() ;
+
+
+    if(mark[n]==0) cout<<"Prime number " ;
+    else cout<<"Not a prime number " ;
 }
