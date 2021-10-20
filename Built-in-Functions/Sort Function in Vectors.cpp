dac76c15a69fa6b8cad442f86ca8b4ef15ee591a
Sabrina Mostafa <78508655+sabrina-mostafa@users.noreply.github.com>
2021-10-20 17:42:15 +0600

    Create Sort Function in Vectors.cpp

---
+++ Built-in-Functions/Sort Function in Vectors.cpp
@@ -0,0 +1,61 @@
+///  ~~~~~Bismillah'hir Rah'manir Rahim~~~~~  ///
+
+/*  "  Allah(SWT) blesses with knowledge whom he wants. "   */
+/*  " Indeed with Hardship, comes Ease. "    */
+
+
+#include<bits/stdc++.h>
+using namespace std ;
+
+#define  Fast      ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ULL       unsigned long long
+#define  LL         long long
+#define ST          string
+#define  F           first
+#define  S           second
+#define  PB        push_back
+#define  MP       make_pair
+#define  pi         acos(-1.0)
+#define  NL       "\n"
+#define   _          " "
+
+/// ---for Unordered map---
+/* mpp.max_load_factor(0.25); mpp.reserve(1024); */
+/* cout << fixed << setprecision(12);*/
+/// ---for Setprecision---
+/* cout<< fixed << showpoint << setprecision(9) << */
+
+int dx4 [ ] = {-1,   0,   0,  +1} ;
+int dy4 [ ] = { 0,  -1,  +1,  0} ;
+int dx [ ] = {-1,  -1,  -1,   0,   0,  +1,  +1,  +1} ;
+int dy [ ] = {-1,   0,  +1,  -1,  +1,  -1,   0,  +1} ;
+
+
+int main ()
+{
+    int n ;
+    cin>> n;
+    vector<int> v(n) ;
+
+    for(int i=0; i<n; i++)
+        cin>>v[i] ;
+    sort(v.begin(), v.end()) ;
+    for(int i=0; i<n; i++)
+        cout<<v[i]<<_ ;
+    cout<<NL ;
+
+    for(int i=0; i<n; i++)
+        cin>>v[i] ;
+    sort(v.begin(), v.begin()+3) ;
+    for(int i=0; i<n; i++)
+        cout<<v[i]<<_ ;
+    cout<<NL ;
+
+    for(int i=0; i<n; i++)
+        cin>>v[i] ;
+    sort(v.begin(), v.end(), greater<int>() ) ;
+    for(int i=0; i<n; i++)
+        cout<<v[i]<<_ ;
+    cout<<NL ;
+    return 0 ;
+}
